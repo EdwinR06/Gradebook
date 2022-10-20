03647196c93325df559bf045b2308da845e62353
Edwin Ryerson <eryerson06@gmail.com>
2022-10-19 23:28:05 -0600

    Add Teacher Functionality

--- teacher.py
+++ teacher.py
@@ -1,14 +1,14 @@
 import student
 
-def assignment_maker(name, assignment_points_earned, assignment_points_available):
-    res = input('Would you like to add another assignment? \n[a] Yes \n[b] No \n')
+
+
+def assignment_maker(student):
+    res = input('Would you like to add an assignment? \n[a] Yes \n[b] No \n')
     if res == 'a':
         res1 = (input('What is the name of this assignment?\n')).capitalize()
         res2 = float(input('Available Points: '))
         res3 = float(input('Earned Points: '))
-        assignment_points_available += res2
-        assignment_points_earned += res3
-        assignment_maker(name, assignment_points_earned, assignment_points_available)
+        student.assignments.make_assignment(res1, res3, res2)
+        assignment_maker(student)
     else:
-        student1 = student.Student(name, assignment_points_available, assignment_points_earned)
-        print('{} earned a {}% in this class.'.format((student1.name).capitalize(), str(student1.grade)))
+        print(student.assignments.final_grade())
