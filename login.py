8e83d805505ac103b71681fd9b6270e14dbd8931
Edwin Ryerson <eryerson06@gmail.com>
2022-10-20 12:18:55 -0600

    Add Current/New Student Functionality for Teachers

--- login.py
+++ login.py
@@ -5,8 +5,16 @@ from student import students1
 res = input('Are you a teacher or student? \n[a] Teacher \n[b] Student \n')
 if res == 'a':
     res_name = input("What is the student's name? \n")
-    student = Student(res_name)
-    assignment_maker(student)
+    in_students = False
+    for i in students1.students:
+        if res_name == i[0].name:
+            in_students = True
+            assignment_maker(i[0])
+
+    if(in_students == False):
+        student = Student(res_name)
+        assignment_maker(student)
+        
 if res == 'b':
     res_name = input("What is your name? \n")
     for i in students1.students:
