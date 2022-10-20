10a77d0f435a978be91148ac8f2a15412f286d08
Edwin Ryerson <eryerson06@gmail.com>
2022-10-20 13:57:12 -0600

    Add Protection Against Invalid Grade Inputs

--- teacher.py
+++ teacher.py
@@ -7,7 +7,13 @@ def assignment_maker(student):
     if res == 'a':
         res1 = (input('What is the name of this assignment?\n')).capitalize()
         res2 = float(input('Available Points: '))
+        if res2 <= 0:
+            print("Invalid response: please input a value greater than zero")
+            res2 = float(input('Available Points: '))
         res3 = float(input('Earned Points: '))
+        if res3 < 0:
+            print("Invalid response: please input a value greater than or equal to zero")
+            res3 = float(input('Earned Points: '))
         student.assignments.make_assignment(res1, res3, res2)
         assignment_maker(student)
     else:
