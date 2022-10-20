6e1f8a5fb7a0c7c8803d4e24d38c0a6aaeb8ff52
Edwin Ryerson <eryerson06@gmail.com>
2022-10-20 12:37:50 -0600

    Add Student Assignment Score Check Functionality

--- login.py
+++ login.py
@@ -14,10 +14,23 @@ if res == 'a':
     if(in_students == False):
         student = Student(res_name)
         assignment_maker(student)
-        
+
 if res == 'b':
     res_name = input("What is your name? \n")
+    in_students = False
     for i in students1.students:
         if res_name == i[0].name:
-            print("{} has a {}% in class.".format(i[0].name, i[1]))
+            in_students = True
+            res_choice = input("Would you like to check your final grade or an assignment grade?\n[a] Final Grade \n[b] Assignment Grade \n")
+            if res_choice == 'a':
+                print("{} has a {}% in class.".format(i[0].name, i[1]))
+            elif res_choice == 'b':
+                res_assignment = input("Which assignment?\n")
+                for j in i[0].assignments.assignment_list:
+                    if j[0] == res_assignment:
+                        print("{} got a {}% on {}.".format(i[0].name, j[1][0]/j[1][1]*100, j[0]))
+
+    if(in_students == False):
+        print("Student does not exist.")
+
 
