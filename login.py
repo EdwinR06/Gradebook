f5f9eb4f9fd3967e82a945b0eba4d2f62ebb033a
Edwin Ryerson <eryerson06@gmail.com>
2022-10-20 11:42:31 -0600

    Add Student Grade Check Functionality

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 from student import Student
 from teacher import assignment_maker
-from student import students
+from student import students1
 
 res = input('Are you a teacher or student? \n[a] Teacher \n[b] Student \n')
 if res == 'a':
@@ -9,7 +9,7 @@ if res == 'a':
     assignment_maker(student)
 if res == 'b':
     res_name = input("What is your name? \n")
-    for i in students.students:
-        if res_name == i[0]:
-            print(i[1])
+    for i in students1.students:
+        if res_name == i[0].name:
+            print("{} has a {}% in class.".format(i[0].name, i[1]))
 
