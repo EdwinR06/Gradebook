5afa27ca879cbf6aebcb42fb383da1a6b38f7001
Edwin Ryerson <eryerson06@gmail.com>
2022-10-19 23:28:39 -0600

    Add Login Logic

--- login.py
+++ login.py
@@ -1,5 +1,15 @@
-from student import create_student
+from student import Student
+from teacher import assignment_maker
+from student import students
 
 res = input('Are you a teacher or student? \n[a] Teacher \n[b] Student \n')
 if res == 'a':
-    create_student()
+    res_name = input("What is the student's name? \n")
+    student = Student(res_name)
+    assignment_maker(student)
+if res == 'b':
+    res_name = input("What is your name? \n")
+    for i in students.students:
+        if res_name == i[0]:
+            print(i[1])
+
