73bd57942991248c2b2431c09c06eb94ff2f88d9
Edwin Ryerson <eryerson06@gmail.com>
2022-10-20 14:11:27 -0600

    Add Hardcoded Students

--- student.py
+++ student.py
@@ -37,5 +37,22 @@ bob = Student('Bob')
 bob.assignments.make_assignment('test 1', 90, 100)
 bob.assignments.make_assignment('test 2', 100, 100)
 
+joe = Student('Joe')
+joe.assignments.make_assignment('quiz 1', 20, 25)
+joe.assignments.make_assignment('test 1', 100, 100)
+joe.assignments.make_assignment('quiz 2', 25, 25)
+
+mary = Student('Mary')
+mary.assignments.make_assignment('quiz 1', 23, 25)
+mary.assignments.make_assignment('test 1', 95, 100)
+mary.assignments.make_assignment('quiz 2', 25, 25)
+
+sue = Student('Sue')
+sue.assignments.make_assignment('test 1', 95, 100)
+sue.assignments.make_assignment('test 2', 96, 100)
+
 students1 = Students()
 students1.add_student(bob)
+students1.add_student(joe)
+students1.add_student(mary)
+students1.add_student(sue)
